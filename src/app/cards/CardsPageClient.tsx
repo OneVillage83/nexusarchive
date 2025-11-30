c5fea648d6a51ce2e60d2178e9e2d21e0f038e11
Francisco Rodriguez <f_rodriguez91@yahoo.com>
2025-11-29 18:56:55 -0800

    same as before

--- src/app/cards/CardsPageClient.tsx
+++ src/app/cards/CardsPageClient.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 type Card = {
   id: number;
